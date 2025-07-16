832b013fec8bfc2baab0c2641d3b237f7db56419
Davit043 <dkhachatryan043@gmail.com>
2025-07-16 20:19:12 +0400

    base bubble sort with c#

---
+++ bubble_sort/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace bubble_sort
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] array = [5, 8, 6, 1, 2, 3];
+
+            BubbleSortArr(array);
+
+            Console.WriteLine(String.Join(',', array));
+        }
+        static void BubbleSortArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        swapped = true;
+
+                        int tempValue = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tempValue;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+            return;
+        }
+    }
+}
